73061bcd4572caf82145efca859c01df73bd96c8
Moritz Buhl <git@moritzbuhl.de>
2024-07-10 15:22:11 +0200

    WIP: Add support for QUIC sockets.
    Attempt to support QUIC sockets from https://github.com/lxin/quic
    and add a H3 filter based on curl_ngtcp2.c.
    
    Currently links against libquic instead of using the available TLS code.
    Does not use the send and recv functions from vquic.c, i.e. only uses sendmsg.
    Code needs simplification and clean-up.
    Tests are missing.

--- lib/curl_setup.h
+++ lib/curl_setup.h
@@ -961,7 +961,8 @@ int getpwuid_r(uid_t uid, struct passwd *pwd, char *buf,
 
 #if (defined(USE_NGTCP2) && defined(USE_NGHTTP3)) || \
     (defined(USE_OPENSSL_QUIC) && defined(USE_NGHTTP3)) || \
-    defined(USE_QUICHE) || defined(USE_MSH3)
+    defined(USE_QUICHE) || defined(USE_MSH3) || \
+    (defined(USE_LINUX_QUIC) && defined(USE_NGHTTP3))
 
 #ifdef CURL_WITH_MULTI_SSL
 #error "MultiSSL combined with QUIC is not supported"
--- lib/vquic/vquic.c
+++ lib/vquic/vquic.c
@@ -35,6 +35,7 @@
 #include "dynbuf.h"
 #include "cfilters.h"
 #include "curl_trc.h"
+#include "curl_linux.h"
 #include "curl_msh3.h"
 #include "curl_ngtcp2.h"
 #include "curl_osslq.h"
@@ -63,6 +64,8 @@ void Curl_quic_ver(char *p, size_t len)
   Curl_ngtcp2_ver(p, len);
 #elif defined(USE_OPENSSL_QUIC) && defined(USE_NGHTTP3)
   Curl_osslq_ver(p, len);
+#elif defined(USE_LINUX_QUIC) && defined(USE_NGHTTP3)
+  Curl_linuxq_ver(p, len);
 #elif defined(USE_QUICHE)
   Curl_quiche_ver(p, len);
 #elif defined(USE_MSH3)
@@ -676,6 +679,8 @@ CURLcode Curl_cf_quic_create(struct Curl_cfilter **pcf,
   return Curl_cf_ngtcp2_create(pcf, data, conn, ai);
 #elif defined(USE_OPENSSL_QUIC) && defined(USE_NGHTTP3)
   return Curl_cf_osslq_create(pcf, data, conn, ai);
+#elif defined(USE_LINUX_QUIC) && defined(USE_NGHTTP3)
+  return Curl_cf_linuxq_create(pcf, data, conn, ai);
 #elif defined(USE_QUICHE)
   return Curl_cf_quiche_create(pcf, data, conn, ai);
 #elif defined(USE_MSH3)
@@ -697,6 +702,8 @@ bool Curl_conn_is_http3(const struct Curl_easy *data,
   return Curl_conn_is_ngtcp2(data, conn, sockindex);
 #elif defined(USE_OPENSSL_QUIC) && defined(USE_NGHTTP3)
   return Curl_conn_is_osslq(data, conn, sockindex);
+#elif defined(USE_LINUX_QUIC) && defined(USE_NGHTTP3)
+  return Curl_conn_is_linuxq(data, conn, sockindex);
 #elif defined(USE_QUICHE)
   return Curl_conn_is_quiche(data, conn, sockindex);
 #elif defined(USE_MSH3)
